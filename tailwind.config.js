3a7969998f8dc53c1f1a20bd30d365914e4518e5
syazantri <syazantri@gmail.com>
2024-04-26 20:24:06 +0700

    feat: create homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', defaultTheme.fontFamily.sans],
+        // font2: ['', '']// 
+      },
+    },
   },
   plugins: [],
 }
