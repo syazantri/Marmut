1735070913a270cdb05d3846f34bb73e6d874c6e
rifdahnabilah23 <rifdah.nabilah@ui.ac.id>
2024-04-28 22:18:07 +0700

    bagian biru kelar

--- podcast_chart/urls.py
+++ podcast_chart/urls.py
@@ -4,8 +4,11 @@ from podcast_chart.views import *
 app_name = 'podcast_chart'
 
 urlpatterns = [
-    path('play_podcast/', play_podcast, name='play_podcast'),
-    path('play_user_playlist/', play_user_playlist, name='play_user_playlist'),
-    path('lihat_chart/', lihat_chart, name='lihat_chart'),
-    path('detail_chart/', detail_chart, name='detail_chart'),
+    path('play-podcast/', play_podcast, name='play_podcast'),
+    path('create-podcast/', create_podcast, name='create_podcast'),
+    path('lihat-chart/', lihat_chart, name='lihat_chart'),
+    path('detail-chart/', detail_chart, name='detail_chart'),
+    path('list-podcast/', list_podcast, name='list_podcast'),
+    path('create-episode/', create_episode, name='create_episode'),
+    path('list-episode/', list_episode, name='list_episode'),
 ]
--- podcast_chart/views.py
+++ podcast_chart/views.py
@@ -4,11 +4,20 @@ from django.shortcuts import render
 def play_podcast(request):
     return render(request, 'play_podcast.html')
 
-def play_user_playlist(request):
-    return render(request, 'play_user_playlist.html')
+def create_podcast(request):
+    return render(request, 'create_podcast.html')
 
 def lihat_chart(request):
     return render(request, 'lihat_chart.html')
 
 def detail_chart(request):
     return render(request, 'detail_chart.html')
+
+def list_podcast(request):
+    return render(request, 'list_podcast.html')
+
+def create_episode(request):
+    return render(request, 'create_episode.html')
+
+def list_episode(request):
+    return render(request, 'list_episode.html')
