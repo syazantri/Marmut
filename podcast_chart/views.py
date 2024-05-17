642046a63963003d3204e89ca6d533ad613be2f1
rifdahnabilah23 <rifdah.nabilah@ui.ac.id>
2024-05-17 14:46:15 +0700

    Kelar fitur Chart

--- podcast_chart/views.py
+++ podcast_chart/views.py
@@ -115,7 +115,62 @@ def lihat_chart(request):
 
 
 def detail_chart(request):
-    return render(request, 'detail_chart.html')
+    tipe_top = request.GET.get('tipe_top')
+    with connection.cursor() as cursor:
+        if tipe_top == "Daily Top 20":
+            cursor.execute("""
+                SELECT konten.id, konten.judul AS "Judul Lagu", akun.nama AS "Oleh", konten.tanggal_rilis AS "Tanggal Rilis", COUNT(*) AS "Total Plays"
+                FROM akun_play_song
+                JOIN song ON akun_play_song.id_song = song.id_konten JOIN konten ON song.id_konten = konten.id JOIN artist ON song.id_artist = artist.id JOIN akun ON artist.email_akun = akun.email
+                WHERE akun_play_song.waktu::date = CURRENT_DATE
+                GROUP BY konten.id, konten.judul, akun.nama, konten.tanggal_rilis
+                ORDER BY "Total Plays" DESC
+                LIMIT 20;
+            """)
+        elif tipe_top == "Weekly Top 20":
+            cursor.execute("""
+                SELECT konten.id, konten.judul AS "Judul Lagu", akun.nama AS "Oleh", konten.tanggal_rilis AS "Tanggal Rilis", COUNT(*) AS "Total Plays"
+                FROM akun_play_song
+                JOIN song ON akun_play_song.id_song = song.id_konten JOIN konten ON song.id_konten = konten.id JOIN artist ON song.id_artist = artist.id JOIN akun ON artist.email_akun = akun.email
+                WHERE akun_play_song.waktu >= date_trunc('week', CURRENT_DATE)
+                GROUP BY konten.id, konten.judul, akun.nama, konten.tanggal_rilis
+                ORDER BY "Total Plays" DESC
+                LIMIT 20;
+            """)
+        elif tipe_top== "Monthly Top 20":
+            cursor.execute("""
+                SELECT konten.id, konten.judul AS "Judul Lagu", akun.nama AS "Oleh", konten.tanggal_rilis AS "Tanggal Rilis", COUNT(*) AS "Total Plays"
+                FROM akun_play_song
+                JOIN song ON akun_play_song.id_song = song.id_konten JOIN konten ON song.id_konten = konten.id JOIN artist ON song.id_artist = artist.id JOIN akun ON artist.email_akun = akun.email
+                WHERE akun_play_song.waktu >= date_trunc('month', CURRENT_DATE)
+                GROUP BY konten.id, konten.judul, akun.nama, konten.tanggal_rilis
+                ORDER BY "Total Plays" DESC
+                LIMIT 20;
+            """)
+        elif tipe_top == "Yearly Top 20":
+            cursor.execute("""
+                SELECT konten.id, konten.judul AS "Judul Lagu", akun.nama AS "Oleh", konten.tanggal_rilis AS "Tanggal Rilis", COUNT(*) AS "Total Plays"
+                FROM akun_play_song
+                JOIN song ON akun_play_song.id_song = song.id_konten JOIN konten ON song.id_konten = konten.id JOIN artist ON song.id_artist = artist.id JOIN akun ON artist.email_akun = akun.email
+                WHERE akun_play_song.waktu >= date_trunc('year', CURRENT_DATE)
+                GROUP BY konten.id, konten.judul, akun.nama, konten.tanggal_rilis
+                ORDER BY "Total Plays" DESC
+                LIMIT 20;
+            """)
+        else:
+            return render(request, '404.html')
+        song_favorit = cursor.fetchall()
+    context = {
+        'judul' : tipe_top,
+        'list_song' : [
+            {
+                'id_lagu': row[0], 
+                'title': row[1], 
+                'artist': row[2], 
+                'release_date': row[3].strftime('%d/%m/%Y'), 
+                'total_plays': row[4]} for row in song_favorit]
+    }
+    return render(request, 'detail_chart.html', context)
 
 def list_podcast(request):
     email = request.COOKIES.get("email")
